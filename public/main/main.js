046f334e02d2e07cefdcbd13c5c15fdf0c63be10
davidyeong97 <davidyeong97@gmail.com>
2021-05-16 19:26:19 +0800

    update

--- public/main/main.js
+++ public/main/main.js
@@ -23,49 +23,54 @@ const toggleAdd = () => {
     addButton.classList.toggle('displayNone')
 }
 
+backButton2.addEventListener("click", toggleAdd)
+addButton.addEventListener("click", toggleAdd)
+
 // GET
 async function renderContentPage() {
     const loadData = await fetch(url)
     const jsonData = await loadData.json()
     const updatedhtml = jsonData.reduce((acc, element) => {
         return acc + `
-        <div class="container-fluid" data-id="${element.id}" >
-        <div class="row content-background-1 py-3 py-md-3" style="box-shadow: 0 0 5px #333;">
-        <div class="col d-flex justify-content-center">
-        <button class="toolbutton" id="delete-button"><i class="fas fa-trash delete-icon"></i></button>
-        </div>
-        <div class="col-8 d-flex justify-content-center">
-        <div class="content-text titletext">${element.name}</div>
-        </div>
-        <div class="col d-flex justify-content-center">
-        <button class="toolbutton" id="edit-button"><i class="fas fa-chevron-right edit-icon"></i></button>
-            </div>
-            <div class="container-fluid">
-            <div class="row p-3 mt-3 contenttext">
-            <div class="col">
-            <div class="row d-flex justify-content-center">
-                            ${element.description}
-                            </div>
-                        
-                            <div class="row d-flex justify-content-center content-background-gray">
-                            ${element.assignedto}
-                            </div>
-                            
+        <div class="container-fluid" data-id="${element.id}" data-name="${element.name}" data-description="${element.description}" data-assignedto="${element.assignedto}" data-duedate="${element.duedate}" data-status="${element.status}" >
+            <div class="row content-background-1 py-3 py-md-3" style="box-shadow: 0 0 5px #333;">
+                <div class="col d-flex justify-content-center">
+                    <button class="toolbutton" id="delete-button"><i class="fas fa-trash delete-icon"></i></button>
+                </div>
+                <div class="col-8 d-flex justify-content-center">
+                    <div class="content-text titletext">${element.name}</div>
+                </div>
+                <div class="col d-flex justify-content-center">
+                    <button class="toolbutton" id="edit-button"><i class="fas fa-chevron-right edit-icon"></i></button>
+                </div>
+                <div class="container-fluid">
+                    <div class="row p-3 mt-3 contenttext">
+                        <div class="col">
                             <div class="row d-flex justify-content-center">
-                            ${element.duedate}
+                                ${element.description}
+                            </div>
+                                
+                            <div class="row d-flex justify-content-center content-background-gray">
+                                ${element.assignedto}
+                            </div>
+                                    
+                            <div class="row d-flex justify-content-center">
+                                ${element.duedate}
+                            </div>
+                                
+                            <div class="row d-flex justify-content-center content-background-gray">
+                                ${element.status}
+                            </div>
                         </div>
-                        
-                        <div class="row d-flex justify-content-center content-background-gray">
-                        ${element.status}
-                        </div>
-                        </div>
-                        </div>
-                        </div>
-                        </div>
-                        </div>
-                        `
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
     }, "")
-    dataArea.innerHTML = updatedhtml
+    dataArea.innerHTML = updatedhtml    
+    
+    
 };
 
 renderContentPage()
@@ -74,13 +79,15 @@ renderContentPage()
 addTask.addEventListener('submit', (e) => {
     e.preventDefault();
 
+    let id = '_' + Math.random().toString(36).substr(2, 9);
+
     fetch(url, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({
-                id: 'testing',
+            body: JSON.stringify({ 
+                id: id,
                 name: inputName.value,
                 description: inputDescription.value,
                 assignedto: inputAssignedTo.value,
@@ -109,49 +116,46 @@ dataArea.addEventListener('click', (e) => {
     let id = e.target.parentElement.parentElement.parentElement.dataset.id
 
     if (deleteButtonPressed) {
-        fetch(`${url}/?#${id}`, {
-                method: 'DELETE'
+        fetch(`${url}/${id}`, {
+            method: 'DELETE'
             })
             .then(res => res.json())
-            .then(() => location.reload())
-
+            .then(() => renderContentPage())
     }
 
+    if (editButtonPressed) {
+        toggleAdd()
+        const parent = e.target.parentElement.parentElement.parentElement;
+        let nameContent = parent.dataset.name
+        let descriptionContent = parent.dataset.description
+        let assignedtoContent = parent.dataset.assignedto
+        let duedateContent = parent.dataset.duedate
+        let statusContent = parent.dataset.status
 
+        inputName.value = nameContent
+        inputDescription.value = descriptionContent
+        inputAssignedTo.value = assignedtoContent
+        inputDate.value = duedateContent
+        inputStatus.value = statusContent
+    }
+
+    addSubmitButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        fetch(`${url}/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ 
+                    id: id,
+                    name: inputName.value,
+                    description: inputDescription.value,
+                    assignedto: inputAssignedTo.value,
+                    duedate: inputDate.value,
+                    status: inputStatus.value,
+                })
+        })
+        .then(res => res.json())
+        .then(() => renderContentPage())
+    })
 })
-
-
-
-// const editButton = document.getElementById("edit-button")
-
-// Toggle edit
-
-// const toggleEdit = () => {
-//     document.getElementById('contentPage').classList.toggle('displayNone')
-//     document.getElementById('editPage').classList.toggle('displayNone')
-//     document.getElementById('logout-button').classList.toggle('displayNone')
-//     backButton1.classList.toggle('displayNone')
-//     editButton.classList.toggle('displayNone')
-// }
-
-// editButton.addEventListener("click", toggleEdit)
-// backButton1.addEventListener("click", toggleEdit)
-
-// Toggle add
-
-// const toggleAdd = () => {
-//     document.getElementById('contentPage').classList.toggle('displayNone')
-//     document.getElementById('addPage').classList.toggle('displayNone')
-//     document.getElementById('logout-button').classList.toggle('displayNone')
-//     backButton2.classList.toggle('displayNone')
-//     addButton.classList.toggle('displayNone')
-// }
-
-
-backButton2.addEventListener("click", toggleAdd)
-addButton.addEventListener("click", toggleAdd)
-
-// addSubmitButton.addEventListener("click", async() => {
-// await toggleAdd();
-// await renderContentPage();
-// })
